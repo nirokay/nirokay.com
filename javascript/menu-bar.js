68fb0e85cce3d0357249852c6d618d34ce678f82
Kay <nirokay-public@protonmail.com>
2024-11-09 17:37:26 +0100

    Automated deployment @ Sat Nov  9 17:37:26 CET 2024

--- javascript/menu-bar.js
+++ javascript/menu-bar.js
@@ -3,6 +3,14 @@ const menuId = "id-menu-bar";
 function getMenu() {
     return document.getElementById(menuId);
 }
+function setSelectedToNavigation() {
+    let element = getMenu();
+    if (element == null) {
+        console.error("Could not find navigation menu by id " + menuId);
+        return;
+    }
+    element.selectedIndex = 0;
+}
 function changeToSelectedPage() {
     let element = getMenu();
     if (element == null) {
